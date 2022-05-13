c649b1dc0d1f7525791d029ae67c6225b1ee5d86
dseixas <dseixas247@gmail.com>
2022-05-13 17:03:45 +0100

    App gets random pokemon from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState, useEffect, useCallback } from 'react';
 import './App.css';
 
 function App() {
+  const [randomPokemon, setRandomPokemon] = useState(Math.floor(Math.random() * 898) + 1)
+
+  const [pokemon, setPokemon] = useState({
+    name: undefined,
+    sprite: undefined,
+    gen: undefined,
+    evo: undefined,
+    type1: undefined,
+    type2: undefined,
+    stats: undefined
+  });
+
+  const updatePokemon = useCallback((key, values) => {
+    setPokemon( prevState => ({
+      ...prevState,
+      [key]: values
+    }));
+  }, [pokemon]);
+
+  const axios = require('axios').default;
+
+  useEffect(() => {
+    getPokemonData(randomPokemon);
+  }, [])
+
+  const getPokemonData = (NationalDexNumber) => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/' + NationalDexNumber)
+        .then(function (res) {
+            updatePokemon('name', res.data.name);
+            updatePokemon('sprite', res.data.sprites.front_default);
+
+            updatePokemon('type1', res.data.types[0].type.name);
+            if (res.data.types.length == 2){
+              updatePokemon('type2', res.data.types[1].type.name);
+            }
+            
+            updatePokemon('stats', {
+                hp: res.data.stats[0].base_stat, 
+                attack: res.data.stats[1].base_stat, 
+                defense: res.data.stats[2].base_stat, 
+                spattack: res.data.stats[3].base_stat, 
+                spdefense: res.data.stats[4].base_stat, 
+                speed: res.data.stats[5].base_stat
+            });
+        });
+    
+    axios.get('https://pokeapi.co/api/v2/pokemon-species/' + NationalDexNumber)
+        .then(function (res) {
+            updatePokemon('gen', res.data.generation.name);
+            if (res.data.evolves_from_species == null) {
+              updatePokemon('evo', 'unevolved');
+            }
+            else {
+              updatePokemon('evo', 'evolved');
+            }
+        });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
